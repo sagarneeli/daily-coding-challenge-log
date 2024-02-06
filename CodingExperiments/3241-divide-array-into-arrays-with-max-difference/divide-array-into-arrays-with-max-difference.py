8bbacdb2501837135df337de7bf9ee0b3390b332
Sagar Neeli <sagarneeli1191@gmail.com>
2024-02-05 21:47:05 -0500

    Time: 697 ms (82.74%) | Memory: 30.7 MB (69.47%) - LeetSync

--- CodingExperiments/3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ CodingExperiments/3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -5,5 +5,5 @@ class Solution:
         for i in range(0, len(nums), 3):
             if nums[i + 2] - nums[i] > k:
                 return []
-            ans.append([nums[i], nums[i + 1], nums[i + 2]])
+            ans.append(nums[i : i + 3])
         return ans
