78b096978f6ab2f175aa0489e121d2c1ceb1d4d0
Sagar Neeli <sagarneeli1191@gmail.com>
2024-07-11 05:39:57 -0400

    Time: 45 ms (98.77%) | Memory: 17.8 MB (26.54%) - LeetSync

---
+++ Grind169/1-two-sum/two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = Counter()
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[num] = i
+
+        return [-1, -1]
