d35ff4766f0ba9f8bc993f29b3e83e2dc0579de5
Sagar Neeli <sagarneeli1191@gmail.com>
2024-07-11 06:01:45 -0400

    Time: 41 ms (37.81%) | Memory: 16.5 MB (76.04%) - LeetSync

--- Grind169/21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ Grind169/21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,9 +4,11 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         dummy = curr = ListNode(-1)
-        
+
         while list1 and list2:
             if list1.val < list2.val:
                 curr.next = list1
@@ -14,8 +16,8 @@ class Solution:
             else:
                 curr.next = list2
                 list2 = list2.next
+
             curr = curr.next
-        
+
         curr.next = list1 or list2
         return dummy.next
-        
