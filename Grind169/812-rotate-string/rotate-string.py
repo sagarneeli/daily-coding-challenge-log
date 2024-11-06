22d9f43ca99efd9e35a6d383d89f9e7bf5e811e1
Sagar Neeli <sagarneeli1191@gmail.com>
2024-11-05 23:05:56 -0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (15.83%) - LeetSync

--- Grind169/812-rotate-string/rotate-string.py
+++ Grind169/812-rotate-string/rotate-string.py
@@ -3,20 +3,11 @@ class Solution:
         if len(s) != len(goal):
             return False
 
-        i, n = 0, len(s)
-        """
-        abcde
-        bcdea
-        cdeab
-        """
-        while i < n:
-            left_most_ch = s[0]
-            new_str = s[1:] + left_most_ch
-            print(new_str)
-            if new_str == goal:
+        n = len(s)
+        for _ in range(n):
+            s = s[1:] + s[0]
+            if s == goal:
                 return True
-            s = new_str
-            i = i + 1
 
         return False
             
